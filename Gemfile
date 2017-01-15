dbfd51f025e65cdb356aec09ceca390a0e8fa090
seedy1 <jobe.seedy@yahoo.com>
2017-01-15 00:39:07 +0000

    adding images working

--- Gemfile
+++ Gemfile
@@ -23,6 +23,8 @@ gem 'coffee-rails', '~> 4.2'
 
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'devise'
+gem "paperclip", "~> 5.0.0"
+
 
 
 # Use jquery as the JavaScript library
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170107215452) do
+ActiveRecord::Schema.define(version: 20170113233138) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -26,9 +26,13 @@ ActiveRecord::Schema.define(version: 20170107215452) do
     t.string   "title"
     t.string   "description"
     t.text     "body"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
     t.integer  "user_id"
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
